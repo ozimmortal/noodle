8731dba992a80655f737a85a5d893092dcc1f376
oz <zaleambo@gmail.com>
2026-08-04 14:13:04 +0300

    Refactor project structure

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ from textual.app import App
 from pages.home import HomeScreen
 
 
-class BellyApp(App):
+class NoodleApp(App):
     CSS_PATH = "styles/app.tcss"
     SCREENS = {"home": HomeScreen}
 
     def on_mount(self):
         self.push_screen("home")
 
-
-if __name__ == "__main__":
-    app = BellyApp()
+def main():
+    app = NoodleApp()
     app.run()
+if __name__ == "__main__":
+    main()
